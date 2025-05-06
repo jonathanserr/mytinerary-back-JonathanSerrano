4e2f366df046e6da799ea615ff60c27c0ca44565
jonathan_serrano <jonathan_serrano@hotmail.com>
2025-05-05 22:30:39 -0500

    correction sprint 3

--- router/itinerariesR.js
+++ router/itinerariesR.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {allItineraries,itinerariesByCity,itinerariesByID } from "../controllers/itineraries/read.js";
+import {allItineraries,itinerariesByCityId,itinerariesByID } from "../controllers/itineraries/read.js";
 import {create,createAll} from "../controllers/itineraries/create.js";
 import  {deleteItinerary}  from "../controllers/itineraries/delete.js";
 import update from "../controllers/cities/update.js";
@@ -7,7 +7,7 @@ import update from "../controllers/cities/update.js";
 const routerItinerary = Router();
 
 routerItinerary.get("/allItineraries", allItineraries); // Get all itineraries
-routerItinerary.get("/city/:id", itinerariesByCity); // Get itineraries by city ID
+routerItinerary.get("/city/:id", itinerariesByCityId); // Get itineraries by city ID
 routerItinerary.get("/id/:id", itinerariesByID); // Get itinerary by ID
 
 routerItinerary.post('/create',create)
